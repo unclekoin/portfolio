b9363aa6b343fdb0a3d2a0e22998b81078cd915f
Pavel Koryakin <unclekoin@gmail.com>
2022-02-01 13:16:35 +0300

    feat: add skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
+const skillsContent = document.querySelectorAll('.skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
+const skillsContainer = document.querySelector('.skills__container');
 
 if (navToggle) {
   navToggle.addEventListener('click', () => {
@@ -15,14 +18,26 @@ if (navClose) {
 }
 
 navMenu.addEventListener('click', ({ target }) => {
-  if (target.className.includes('nav__link') || target.className.includes('nav__icon')) {
+  if (target.classList.contains('nav__link')) {
     navMenu.classList.remove('show-menu');
   }
 })
 
+function toggleSkills() {
+  const itemClass = this.parentNode.className
 
-/*==================== ACCORDION SKILLS ====================*/
+  skillsContent.forEach((item) => {
+    item.className = 'skills__content skills__close'
+  })
 
+  if (itemClass === 'skills__content skills__close') {
+    this.parentNode.className = 'skills__content skills__open'
+  }
+}
+
+skillsHeader.forEach((element) => {
+  element.addEventListener('click', toggleSkills);
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
